27faaca2d510b5effb9abc4f3762c61d4012ecca
pse918 <75546410+pse918@users.noreply.github.com>
2021-12-03 12:24:02 -0500

    Delete Main.java

--- src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Day1 day1 = new Day1();
-        day1.countIncreases(day1.depthsArray);
-        day1.countTriadIncreases();
-    }
-}
